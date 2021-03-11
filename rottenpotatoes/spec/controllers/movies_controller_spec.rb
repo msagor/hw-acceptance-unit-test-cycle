a21909235bc12dfd6b053e7ef2db8364cd58d692
EC2 Default User <ec2-user@ip-172-31-6-8.us-east-2.compute.internal>
2021-03-11 22:51:04 +0000

    part 3 80% code coverage

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -36,6 +36,50 @@ RSpec.describe MoviesController, type: :controller do
         end
     end
     
+    describe "#edit" do
+        it "editing existing movie" do
+            movie0 = Movie.create(title: 'movie0', director: 'director0', rating:'G', release_date: '2021-01-01')
+            get :edit, id: movie0.id
+            expect(response).to render_template :edit
+        end
+    end
+    
+    describe "#update" do
+        it "updating existing movie" do
+            movie0 = Movie.create(title: 'movie', director: 'director', rating:'G', release_date: '2021-00-00')
+            put :update, id: movie0.id, movie: {title: 'movie', director: 'director', rating:'R', release_date: '2021-04-01'}
+            movie0.reload
+            expect(movie0.title).to eq('movie')
+            expect(movie0.director).to eq('director')
+            expect(movie0.rating).to eq('R')
+            expect(movie0.release_date).to eq('2021-04-01')
+            expect(flash[:notice]).to eq("#{movie0.title} was successfully updated.")
+            expect(response).to redirect_to(movie0)
+        end
+    end
+
+    describe "#create" do
+        it "creating new movie" do
+            count= Movie.count
+            post :create, movie: {title: 'new_movie', director: 'new_director', rating:'R', release_date: '2021-3-11'}
+            expect(flash[:notice]).to eq "new_movie was successfully created."
+            expect(Movie.count).to eq(count+1)
+            expect(response).to redirect_to(movies_path)
+        end 
+    end
+
+    describe "#destroy" do
+        it "destroying the movie" do
+            movie_0 = Movie.create(title: 'movie', director: 'director', rating:'G', release_date: '2021-01-01')
+            count= Movie.count
+            delete :destroy, id: movie_0.id
+            expect(flash[:notice]).to eq "Movie 'movie' deleted."
+            expect(Movie.count).to eq(count-1)
+            expect(response).to redirect_to movies_path
+        end
+    end
+    
+    
 
         
 end
